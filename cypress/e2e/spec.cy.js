a15ff4ca0b2f756b2115e9f5225396030ee2f92f
mariaaa <toledomiranda.maria23@gmail.com>
2025-08-13 16:47:12 -0400

    lista de objetos com json

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,15 +8,63 @@ describe('template spec', () => {
     cy.wait(500)
     cy.get('#btn_login').click()
  
-    const usuarios = [];
-    for (let i = 1; i <= 10; i++){
-      usuarios.push({
-        id: i,
-        nome: `usuario ${i}`,
-        senha: `senha${i}`
-      });
-    }
+    const user_json = `[
+      {
+        "id": 1,
+        "nome": "Lucas",
+        "senha": "k2l9x8q1"
+      },
+      {
+        "id": 2,
+        "nome": "Mariana",
+        "senha": "z7m3t5r8"
+      },
+      {
+        "id": 3,
+        "nome": "Carlos",
+        "senha": "v8c4b1n2"
+      },
+      {
+        "id": 4,
+        "nome": "Beatriz",
+        "senha": "d9f6s3h0"
+      },
+      {
+        "id": 5,
+        "nome": "Rafael",
+        "senha": "m1n8k2j7"
+      },
+      {
+        "id": 6,
+        "nome": "Fernanda",
+        "senha": "p4o6l9u3"
+      },
+      {
+        "id": 7,
+        "nome": "João",
+        "senha": "s0x3y7z1"
+      },
+      {
+        "id": 8,
+        "nome": "Aline",
+        "senha": "w5e8d6q9"
+      },
+      {
+        "id": 9,
+        "nome": "Thiago",
+        "senha": "t3r9g2m5"
+      },
+      {
+        "id": 10,
+        "nome": "Camila",
+        "senha": "h8y2b4k6"
+      }
+    ]`;
+
+    const usuarios = JSON.parse(user_json);
+  
     cy.get('body')
+    
     usuarios.forEach((usuario, index) => {
       cy.get('#nome').type(usuario.nome)
       cy.wait(500)
